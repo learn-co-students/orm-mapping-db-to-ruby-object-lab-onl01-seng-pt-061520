f2f43ef46a5604022a6d9d36a8f2a0eed50747e9
Cameron Hudson <camnn3@yahoo.com>
2020-09-04 17:33:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,7 +98,7 @@ def self.first_X_students_in_grade_10(num)
   
   DB[:conn].execute(sql, name).map do |row|
         self.new_from_db(row)
-     end.first
+     end
    end
    
    def self.first_student_in_grade_10
