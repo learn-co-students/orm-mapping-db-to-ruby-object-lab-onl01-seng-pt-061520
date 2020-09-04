a3f80465907a7ba4f97d66a4d212adbaad51ae0f
Cameron Hudson <camnn3@yahoo.com>
2020-09-04 17:17:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,8 +75,9 @@ end
   
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT COUNT(grade > 10)
+      SELECT COUNT *
       FROM students
+      WHERE grade < 12
     SQL
     
     DB[:conn].execute(sql)
