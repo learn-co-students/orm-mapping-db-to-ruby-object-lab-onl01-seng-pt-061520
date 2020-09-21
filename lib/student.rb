46e12da1e2f2b5aa0051a833c78323d956c5ab8d
idalispaz <idalispaz@gmail.com>
2020-09-21 14:50:08 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row) 
+    # create a new Student object given a row from the database
     new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
@@ -10,6 +11,8 @@ class Student
   end
 
   def self.all
+    # retrieve all the rows from the "Students" database
+    # remember each row should be a new instance of the Student class
     sql = <<-SQL
       SELECT * FROM students
     SQL
@@ -19,7 +22,8 @@ class Student
   end
 
   def self.find_by_name(name)
-  
+    # find the student in the database given a name
+    # return a new instance of the Student class
     sql = <<-SQL
       SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
@@ -51,7 +55,9 @@ class Student
   end
 
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
+    sql = <<-SQL
+    DROP TABLE IF EXISTS students
+    SQL
     DB[:conn].execute(sql)
   end
 
